a56f63690dfd19c32e6cff8a925d1d39f670b8e8
WzFFzW <397788323@qq.com>
2019-07-20 14:45:37 +0800

    题目七修改

--- 题目七/reverse.js
+++ 题目七/reverse.js
@@ -3,23 +3,24 @@
  * @return {number}
  */
 var reverse = function(x) {
-  function round_10(num) {
-    return Math.round(num / 10);
+  function floor_10(num) {
+    return parseInt(num / 10);
   }
-  const res = 0;
+  let res = 0;
   const MAX = Math.pow(2, 31) - 1;
   const MIN = -MAX - 1;
-  while (x) {
+  while (Math.abs(x) !== 0) {
     const pop = x % 10;
-    if (res > round_10(MAX) || (res === round_10(MAX) && pop > 7)) {
+    if (res > floor_10(MAX) || (res === floor_10(MAX) && pop > 7)) {
       return 0;
     }
-    if (res < round_10(MIN) || (res === round_10(MIN) && pop < -8)) {
+    if (res < floor_10(MIN) || (res === floor_10(MIN) && pop < -8)) {
       return 0;
     }
-    x = round_10(x);
+    res = res * 10 + pop;
+    x = floor_10(x);
   }
   return res;
 };
 
-console.log(reverse(-312));
+console.log(reverse(0));
