213a572b1e2eaf6c44522b5c74e78c3cc745854b
WzFFzW <397788323@qq.com>
2019-07-20 14:48:25 +0800

    fix: 修改题目一

--- 题目一/twoNum.js
+++ 题目一/twoNum.js
@@ -7,7 +7,7 @@ var twoSum = function(nums, target) {
   let i = 0;
   while (i < nums.length) {
       const other_index = nums.indexOf(target - nums[i]);
-      if (other_index > -1 && i !== other_index) {
+      if (other_index > -1 && i !== other_index && nums.indexOf(target - nums[i], other_index) > -1) {
           return [i, other_index]
       }
       i++;
