519d6288aa321105af01afb545e6efc2cb69d61c
WzFFzW <397788323@qq.com>
2019-09-24 09:49:53 +0800

    72

--- dp/72.js
+++ dp/72.js
@@ -4,18 +4,34 @@
  * @return {number}
  */
 var minDistance = function(word1, word2) {
-  // let dist = [];
+  if (word1 === word2) {
+    return 0;
+  }
+  if (word1 === '' || word2 === '') {
+    return word2.length || word1.length;
+  }
+  let dist = [];
+  for (let i = 0; i < word1.length; i++) {
+    dist[i] = new Array(word2.length);
+  }
   function dp(i, j) {
-    if (i <= 0 && j <= 0) {
-      return 1;
+    if (i === -1) {
+      return j + 1;
+    }
+    if (j === -1) {
+      return i + 1;
+    }
+    if (dist[i][j]) {
+      return dist[i][j];
     }
     if (word1[i] === word2[j]) {
-      return dp(i - 1, j - 1);
+      return dist[i][j] = dp(i - 1, j - 1);
     }
     const insert_step = dp(i, j - 1) + 1;
     const delete_step = dp(i - 1, j) + 1;
     const replace_step = dp(i - 1, j - 1) + 1;
-    return Math.min(dp(insert_step, delete_step, replace_step));
+    return dist[i][j] = Math.min(delete_step, insert_step, replace_step);
   }
-  return dp(word1.length - 1, word2.length - 1);
+  const a = dp(word1.length - 1, word2.length - 1);
+  return a;
 };
